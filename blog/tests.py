8dcca9c120d495bf6aa0d44552645fc3f83c7ec3
nagi930 <gnkim827@gmail.com>
2021-01-02 15:04:09 +0900

    category add

--- blog/tests.py
+++ blog/tests.py
@@ -1,16 +1,80 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from .models import Post, Category
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-# Create your tests here.
+
+def create_category(name='life', description=''):
+    category, is_created = Category.objects.get_or_create(
+        name=name,
+        description=description
+    )
+
+    category.slug = category.name.replace(' ', '-').replace('/', '')
+    category.save()
+
+    return category
+
+def create_post(title, content, author, category=None):
+    blog_post = Post.objects.create(
+        title=title,
+        content=content,
+        created=timezone.now(),
+        author=author,
+        category=category
+    )
+
+    return blog_post
+
+
+
+class TestModel(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='user', password='password')
+
+    def test_category(self):
+        category = create_category()
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World',
+            author=self.author_000,
+            category=category
+        )
+
+        self.assertEqual(category.post_set.count(), 1)
+
+
+        def test_post(self):
+            post_000 = create_post(
+                title='The first post',
+                content='Hello World',
+                author=self.author_000,
+                category=category
+            )
+
+
+
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
         self.author_000 = User.objects.create(username='user', password='password')
 
-    def test_post_list(self):
+    def check_navbar(self, soup):
+        navbar = soup.find('div', id='navbar')
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About me', navbar.text)
+
+    def check_right_side(self, soup):
+        category_card = soup.find('div', id='category-card')
+        self.assertIn('미분류(1)', category_card.text)
+        self.assertIn('News(1)', category_card.text)
+
+
+
+
+    def test_post_list_no_post(self):
         response = self.client.get('/blog/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -18,20 +82,27 @@ class TestView(TestCase):
 
         print(title.text)
 
-        self.assertEqual(title.text, 'Blog')
+        self.assertIn(title.text, 'Blog')
 
-        navbar = soup.find('div', id='navbar')
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About me', navbar.text)
+        self.check_navbar(soup)
+        # navbar = soup.find('div', id='navbar')
+        # self.assertEqual(Post.objects.count(), 0)
+        # self.assertIn('아직 게시물이 없습니다.', soup.body.text)
 
-        self.assertEqual(Post.objects.count(), 0)
-        self.assertIn('아직 게시물이 없습니다.', soup.body.text)
 
-        post_000 = Post.objects.create(
+    def test_post_list_with_post(self):
+
+        post_000 = create_post(
             title = 'The first post',
             content = 'Hello World',
-            created = timezone.now(),
             author = self.author_000
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Hello World2',
+            author=self.author_000,
+            category=create_category(name='News')
         )
 
         self.assertGreater(Post.objects.count(), 0)
@@ -43,15 +114,102 @@ class TestView(TestCase):
         self.assertNotIn('아직 게시물이 없습니다.', soup.body.text)
         self.assertIn(post_000.title, body.text)
 
+        post_000_read_more_bnt = body.find('a', id='read-more-post-{}'.format(post_000.pk))
+        self.assertEqual(post_000_read_more_bnt['href'], post_000.get_absolute_url())
+
+        # category card에서
+        #### 미분류(1), News(1) 있어야함
+        self.check_right_side(soup)
+
+        main_div = soup.find('div', id='main-div')
+        self.assertIn('News', main_div.text)
+        self.assertIn('미분류', main_div.text)
+
     def test_post_detail(self):
-        post_000 = Post.objects.create(
+        post_000 = create_post(
             title='The first post',
             content='Hello World',
-            created=timezone.now(),
             author=self.author_000
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Hello World2',
+            author=self.author_000,
+            category=create_category(name='News')
         )
 
         self.assertGreater(Post.objects.count(), 0)
 
         post_000_url = post_000.get_absolute_url()
-        self.assertEqual(post_000_url, '/blog/{}'.format(post_000.pk))
+        self.assertEqual(post_000_url, '/blog/{}/'.format(post_000.pk))
+
+        response = self.client.get(post_000_url)
+
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.title
+
+        self.assertEqual(title.text, '{} - Blog'.format(post_000.title))
+
+        self.check_navbar(soup)
+
+        body = soup.body
+        main_div = body.find('div', id='main-div')
+        self.assertIn(post_000.title, main_div.text)
+        self.assertIn(post_000.author.username, main_div.text)
+        self.assertIn(post_000.content, main_div.text)
+        self.check_right_side(soup)
+
+    def test_post_list_by_category(self):
+        category_news = create_category(name='News')
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World',
+            author=self.author_000
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Hello World2',
+            author=self.author_000,
+            category=category_news
+        )
+
+        response = self.client.get(category_news.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertEqual('Blog - {}'.format(category_news.name), soup.title.text)
+
+        main_div = soup.find('div', id='main-div')
+        self.assertNotIn('미분류', main_div.text)
+        self.assertIn(category_news.name, main_div.text)
+
+    def test_post_list_no_category(self):
+        category_news = create_category(name='News')
+
+        post_000 = create_post(
+            title='The first post',
+            content='Hello World',
+            author=self.author_000
+        )
+
+        post_001 = create_post(
+            title='The second post',
+            content='Hello World2',
+            author=self.author_000,
+            category=category_news
+        )
+
+        response = self.client.get('/blog/category/_none/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        main_div = soup.find('div', id='main-div')
+        self.assertIn('미분류', main_div.text)
+        self.assertNotIn(category_news.name, main_div.text)
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
@@ -8,9 +8,51 @@ class PostList(ListView):
     def get_queryset(self):
         return Post.objects.order_by('-created')
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostList, self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['post_without_category'] = Post.objects.filter(category=None).count()
+
+        return context
+
 class PostDetail(DetailView):
     model = Post
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(PostDetail, self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['post_without_category'] = Post.objects.filter(category=None).count()
+
+        return context
+
+class PostListByCategory(PostList):
+
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+
+        if slug == '_none':
+            category = None
+        else:
+            category = Category.objects.get(slug=slug)
+
+        return Post.objects.filter(category=category).order_by('-created')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(type(self),self).get_context_data(**kwargs)
+        context['category_list'] = Category.objects.all()
+        context['post_without_category'] = Post.objects.filter(category=None).count()
+
+        slug = self.kwargs['slug']
+        if slug == '_none':
+            category = '미분류'
+        else:
+            category = Category.objects.get(slug=slug)
+            context['category'] = category
+
+
+        context['title'] = 'Blog - {}'.format(category)
+
+        return context
 
 # def post_detail(request, pk):
 #     blog_post = Post.objects.get(pk=pk)
